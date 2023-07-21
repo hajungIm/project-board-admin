572285e603328d48e8e6e6fa602e8cd7b821b3cb
hajungIm <ihj_ewha@ewhain.net>
2023-07-21 17:37:49 +0900

    #4 -  스프링 시큐리티 인증 데이터의 작성
    `Principal`, 카카오 인증 api 의 응답, 그리고 테스트 작성

---
+++ src/test/java/com/springstudy/projectboardadmin/dto/security/KakaoOAuth2ResponseTest.java
@@ -0,0 +1,61 @@
+package com.springstudy.projectboardadmin.dto.security;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("DTO - Kakao OAuth 2.0 인증 응답 데이터 테스트")
+class KakaoOAuth2ResponseTest {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @DisplayName("인증 결과를 Map(deserialized json)으로 받으면, 카카오 인증 응답 객체로 변환한다.")
+    @Test
+    void givenMapFromJson_whenInstantiating_thenReturnsKakaoResponseObject() throws JsonProcessingException {
+        // Given
+        String serializedResponse = """
+                {
+                    "id": 1234567890,
+                    "connected_at": "2023-01-11T00:11:23Z",
+                    "properties": {
+                        "nickname": "홍길동"
+                    },
+                    "kakao_account": {
+                        "profile_nickname_needs_agreement": false,
+                        "profile": {
+                            "nickname": "홍길동"
+                        },
+                        "has_email": true,
+                        "email_needs_agreement": false,
+                        "is_email_valid": true,
+                        "is_email_verified": true,
+                        "email": "test@email.com"
+                    }
+                }
+                """;
+        Map<String, Object> attributes = mapper.readValue(serializedResponse, new TypeReference<>() {});
+
+        // When
+        KakaoOAuth2Response result = KakaoOAuth2Response.from(attributes);
+
+        // Then
+        assertThat(result)
+                .hasFieldOrPropertyWithValue("id", 1234567890L)
+                .hasFieldOrPropertyWithValue("connectedAt", ZonedDateTime.of(2023, 1, 11, 0, 11, 23, 0, ZoneOffset.UTC)
+                        .withZoneSameInstant(ZoneId.systemDefault())
+                        .toLocalDateTime()
+                )
+                .hasFieldOrPropertyWithValue("kakaoAccount.profile.nickname", "홍길동")
+                .hasFieldOrPropertyWithValue("kakaoAccount.email", "test@email.com");
+
+    }
+}
